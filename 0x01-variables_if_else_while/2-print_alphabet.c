773bb8bfbf36f0b1c6526440a342644ed817290f
Codart-7 <iconmoa@gmail.com>
2022-03-12 06:17:33 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints ll the lphabet in lower case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char al;
+
+	al = 'a';
+	while (al <= 'z')
+	{
+		putchar(al);
+		al++;
+	}
+	putchar('\n');
+	
+	return (0);
+}
