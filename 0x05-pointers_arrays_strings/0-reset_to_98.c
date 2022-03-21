058f4048dc493c073c977cd3cfda97c0151568ad
Codart-7 <iconmoa@gmail.com>
2022-03-21 19:08:38 +0100

    return 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * reset to 98 - changes the value of the variable
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is the part where ALX teaches pointers arrays and stuff
