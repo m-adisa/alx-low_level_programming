871569f95af72e1b56e3c38fc390a78633951222
Codart-7 <iconmoa@gmail.com>
2022-03-22 01:16:04 +0100

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,23 +1,24 @@
 #include "main.h"
+
 /**
- * puts2 - main
- * @str: the array to print
+ * puts2 - prints one char out of 2
+ * @str: type
+ *
  */
+
 void puts2(char *str)
 {
-	int i = 0, j  = 0;
+	int i, j = 0;
 
-	while (*str != '\0')
+	while (str[i] != '\0')
 	{
-		str++;
 		i++;
 	}
-	i--;
 
 	while (j < i)
 	{
 		_putchar(str[j]);
-		j += 2;
+		j = j + 2;
 	}
 	_putchar('\n');
 }
