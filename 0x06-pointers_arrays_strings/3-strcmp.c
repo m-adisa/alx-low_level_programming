5cdeae124249bfe34c5d9c92d9eccdfa6fb27da1
Codart-7 <iconmoa@gmail.com>
2022-03-24 17:14:56 +0100

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,12 @@
 #include "main.h"
+
 /**
- *
- *
+ * _strcmp - compare
+ * @s1 : pointerto char params
+ * @s2 : pointer to char params
+ * Return: *dest
  */
+
 int _strcmp(char *s1, char *s2)
 {
 	int i;
