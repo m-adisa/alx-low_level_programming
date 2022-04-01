f6a374e671672d8779a69ed2338e29c2de6bbea8
Codart-7 <iconmoa@gmail.com>
2022-04-01 19:55:27 +0000

    print my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,12 @@
  * main - Entry point
  * @argc: the number of input into main
  * @argv: array containing input into main
+ * Return: 0 -success
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	if (argc >= 1)
+		printf("%s\n", argv[0]);
 
 	return (0);
 }
