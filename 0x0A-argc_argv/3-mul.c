93eb626c94928f946bc4bcd3b9465bc08e1ae5af
Codart-7 <iconmoa@gmail.com>
2022-04-01 20:50:05 +0000

    multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,15 +8,17 @@
  */
 int main(int argc, char *argv[])
 {
-	int sum;
+	int i;
+	int sum = 1;
 
 	if (argc != 3)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
 
-	sum = atoi(argv[1]) * atoi(argv[2]);
+	for (i = 1; i < argc; i++)
+		sum *= atoi(argv[i]);
 
 	printf("%d\n", sum);
 
