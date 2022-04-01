683861cfffcb14f4548814e1bf92edf14ff5e59c
Codart-7 <iconmoa@gmail.com>
2022-04-01 20:38:04 +0000

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: counts input
+ * @argv: array of input
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 1;
+
+	if (argc < 2)
+	{
+		printf("Error");
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+		sum *= atoi(argv[i]);
+
+	printf("%d\n", sum);
+
+	return (0);
+}
