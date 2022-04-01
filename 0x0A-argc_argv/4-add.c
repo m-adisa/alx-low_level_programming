509bf6a4c207e307c78b8fe7292bc9113adc815c
Codart-7 <iconmoa@gmail.com>
2022-04-01 21:44:26 +0000

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,6 +12,12 @@ int main(int argc, char *argv[])
 	int i;
 	int sum = 0;
 
+	if (argc < 2)
+	{
+		printf("%d\n", sum);
+		return (0);
+	}
+
 	for (i = 1; i < argc; i++)
 	{
 		if (!isdigit(argv[i]))
