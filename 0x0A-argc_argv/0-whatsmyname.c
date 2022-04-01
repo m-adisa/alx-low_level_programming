0e9354d95b36b006ffc943a2de7eb89807a955af
Codart-7 <iconmoa@gmail.com>
2022-04-01 19:47:51 +0000

    print my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: the number of input into main
+ * @argv: array containing input into main
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Passing arguments into the main function
