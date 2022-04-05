1580e59901f966f2899b16a1319f9a5488cec852
Codart-7 <iconmoa@gmail.com>
2022-04-05 10:57:30 +0000

    update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,35 +10,28 @@
 int **alloc_grid(int width, int height)
 {
 	int i, j;
-	int **ary;
+	int **grid;
 
-	if (width < 1 || height < 1)
+	if (width + height < 2 || width < 1 || height < 1)
 		return (NULL);
 
-	ary = malloc(sizeof(int *) * height);
-
-	if (ary == NULL)
-	{
-		free(ary);
+	grid = malloc(height * sizeof(*grid));
+	if (grid == NULL)
 		return (NULL);
-	}
 
 	for (i = 0; i < height; i++)
 	{
-		ary[i] = malloc(sizeof(int) * width);
-		if (ary[i] == NULL)
+		grid[i] = malloc(width * sizeof(**grid));
+		if (grid[i] == NULL)
 		{
-			for (i--; i > 0; i--)
-				free(ary[i]);
-			free(ary);
+			for (i--; i >= 0; i--)
+				free(grid[i]);
+			free(grid);
 			return (NULL);
 		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
 	}
 
-	for (i = 0; i < height; i++)
-		for (j = 0; j < width; j++)
-			ary[i][j] = 0;
-
-	return (ary);
-	free(ary);
+	return (grid);
 }
