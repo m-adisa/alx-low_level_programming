4b03a9b0bcc18c5134c7a8b104e4bb447bd93f73
Codart-7 <iconmoa@gmail.com>
2022-04-11 23:17:58 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,33 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+/**
+ * struct dog - stores dog information
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: ownwer of the dog
+ * Description: struct called dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
