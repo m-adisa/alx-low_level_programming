48d36436d28abeab945bf61cfdd1afc1466522b0
Codart-7 <iconmoa@gmail.com>
2022-04-12 02:04:59 +0000

    update

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _FUNCTION_LIKE_MACRO_
-#define _FUNCTION_LIKE_MACRO_
+#ifndef THREE_FUNCTION_LIKE_MACRO_
+#define THREE_FUNCTION_LIKE_MACRO_
 
-#define ABS(x) ((x) < (0) ? ((x) * (-1) : (x))
+#define ABS(x) (x > 0 ? (x) : (x) * -1)
 
 #endif
