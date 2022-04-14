4d705c20208f35e3ea06f41b3fce6e2aa5d039f8
Codart-7 <iconmoa@gmail.com>
2022-04-14 03:13:37 +0000

    update

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds numbers
+ * @a: first number
+ * @b: second number
+ * Return - addition
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subttracts numbers
+ * @a: first number
+ * @b: second number
+ * Return: sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies numbers
+ * @a: first number
+ * @b: second number
+ * Return: multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides numbers
+ * @a: first number
+ * @b: second number
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus of numbers
+ * @a: first number
+ * @b: second number
+ * Return: modulus
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
