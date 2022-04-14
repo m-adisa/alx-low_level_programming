db6fd8a2d5d675be229a3987b7e13b3eb6fdf296
Codart-7 <iconmoa@gmail.com>
2022-04-14 01:37:55 +0000

    search int

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,23 +10,16 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, r;
+	int i;
 
 	if (array && cmp)
 	{
-		if (size > 0)
-		{
-			for (i = 0; i < size; i++)
-			{
-				r = cmp(array[i]);
-				if (!r)
-					return (-1);
-				else
-					return (i);
-			}
-		}
-		else
+		if (size <= 0)
 			return (-1);
+
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
 	}
 
 	return (-1);
