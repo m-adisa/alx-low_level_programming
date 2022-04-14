1572a0db220d01f0b36f62fba59a64c42719ca21
Codart-7 <iconmoa@gmail.com>
2022-04-14 00:57:49 +0000

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
  * array_iterator - executes a funtion on each array element
@@ -10,7 +9,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
 	if (array && size && action)
 	{
