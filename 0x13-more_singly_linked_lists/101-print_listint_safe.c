e72c7e73a23853d36eac1ca48a738e5dbaf15d26
Codart-7 <iconmoa@gmail.com>
2022-04-26 23:26:00 +0000

    update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -56,7 +56,7 @@ size_t print_listint_safe(const listint_t *head)
 			}
 		}
 
-		printf("[%p] %d\n", (void *)head, head->);
+		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
 		nnodes++;
 	}
