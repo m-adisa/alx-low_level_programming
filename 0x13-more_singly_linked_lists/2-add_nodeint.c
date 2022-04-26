daeb86188c0eb0bddd4da2b0d2cc8dc07069194a
Codart-7 <iconmoa@gmail.com>
2022-04-26 12:31:26 +0000

    add node to begining

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,9 +10,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *temp = malloc(sizeof(listint_t));
 
+	if (temp == NULL)
+		return (NULL);
+
 	temp->n = n;
 	temp->next = *head;
-	**head = *temp;
+	*head = temp;
 
 	return (*head);
 }
