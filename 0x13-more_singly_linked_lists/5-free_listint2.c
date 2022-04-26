e2fd2a3eb130cb919d36a29abb0a0e2a92278653
Codart-7 <iconmoa@gmail.com>
2022-04-26 13:35:11 +0000

    free list again

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees the list adn sets the head to NULL
+ * @head: the head of the list
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while ((current = *head) != NULL)
+	{
+		head = head->next;
+		free(current);
+	}
+
+	*head = NULL;
+}
