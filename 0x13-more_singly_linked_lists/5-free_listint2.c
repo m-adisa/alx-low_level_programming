faafea929c6d4690dc6a924991d4ee428a3e7a35
Codart-7 <iconmoa@gmail.com>
2022-04-26 14:02:38 +0000

    free list again

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,13 +7,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current;
+	listint_t *current, *temp;
 
-	while ((current = *head) != NULL)
+	if (head != NULL)
 	{
-		head = head->next;
-		free(current);
+		current = *head;
+		while ((temp = current) != NULL)
+		{
+			current = current->next;
+			free(temp);
+		}
+		*head = NULL;
 	}
-
-	*head = NULL;
 }
