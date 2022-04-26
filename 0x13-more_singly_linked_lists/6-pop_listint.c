fb1870c155505f0c9af8606fdc00c735f5cbadc2
Codart-7 <iconmoa@gmail.com>
2022-04-26 14:51:35 +0000

    pop the head

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head of  list
+ * @head: head of a list
+ * Return: the data stored in the head
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *temp, *prev;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		temp = *head;
+		n = temp->n;
+		prev = temp->next;
+		free(temp);
+		*head = prev;
+	}
+
+	return (n);
+}
