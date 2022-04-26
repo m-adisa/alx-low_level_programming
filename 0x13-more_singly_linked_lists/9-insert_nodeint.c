942ef3c478a60ac4b72a8958e60c2f50d20a727c
Codart-7 <iconmoa@gmail.com>
2022-04-26 19:36:11 +0000

    add node at index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a node
+ * at given position.
+ * @head: head of the list
+ * @idx: where new node should be created
+ * @n: data to be stored at new node
+ * Return: address of new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int count = 0;
+	listint_t *srch, *temp;
+
+	srch = *head;
+
+	if (srch == NULL)
+		return (NULL);
+
+	while ((count < idx) && (srch != NULL))
+	{
+		srch = srch->next;
+		count++;
+	}
+
+	if (srch == NULL)
+		return (NULL);
+
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+		return (NULL);
+
+	temp->n = n;
+
+	if ((count == idx) && (srch != NULL))
+	{
+		temp->next = srch->next;
+		srch->next = temp;
+	}
+
+	return (temp);
+}
