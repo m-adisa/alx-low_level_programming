6777b202496cbec4f2582256fee99e9546948412
Codart-7 <iconmoa@gmail.com>
2022-04-26 13:47:06 +0000

    free list again

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees the list adn sets the head to NULL
+ * free_listint2 - frees the list and sets the head to NULL
  * @head: the head of the list
  * Return: nothing
  */
