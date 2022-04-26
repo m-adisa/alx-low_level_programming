3d46aebe366b7af66ecba71c230dd3e12a4a8ae2
Codart-7 <iconmoa@gmail.com>
2022-04-26 12:10:21 +0000

    print number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked list
+ * @h: the head of the list
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nmem = 0;
+
+	if (h == NULL)
+	{
+		return (nmem);
+	}
+	else
+	{
+		while (h != NULL)
+		{
+			nmem += 1;
+			h = h->next;
+		}
+	}
+	return (nmem);
+}
