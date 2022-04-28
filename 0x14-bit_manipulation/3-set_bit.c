824e978d2fc8bb3ee287bd6f5a61bd6af56b1fb2
Codart-7 <iconmoa@gmail.com>
2022-04-28 20:40:41 +0000

    update solutions

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,7 +6,7 @@
  * @index: index of the bit
  * Return: 1 (success) or -1 (otherwise)
  */
-int set_bit)unsigned long int *n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int m;
 
