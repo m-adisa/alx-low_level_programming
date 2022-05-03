1f24ce67b0ed4a4e3b1c5b9c2479fd93681b5f15
Michael Adisa <57605457+Codart-7@users.noreply.github.com>
2022-05-03 01:45:36 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 
 /**
- * error_file - checks if files can be opened
- * @file_from: file_from
- * @file_to: file_to
- * @argv: arguments vector
- * Return: nothing
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
 void error_file(int file_from, int file_to, char *argv[])
 {
-	if (file_from == 1)
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
@@ -23,10 +23,10 @@ void error_file(int file_from, int file_to, char *argv[])
 }
 
 /**
- * main - Entry Point
- * @argc: number of arguments
- * @argv: arguments vector
- * Return: Always 0
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 	}
 
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND,  0664);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
 	nchars = 1024;
@@ -65,9 +65,8 @@ int main(int argc, char *argv[])
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-
 	return (0);
 }
