cead52a298359d151d0f028b645ba5f251409403
Michael Adisa <57605457+Codart-7@users.noreply.github.com>
2022-05-03 02:04:33 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,25 +1,12 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
 
 #include <stdarg.h>
+#include <stdio.h>
 
-/**
- * struct printer - printer
- * @symbol: symbol
- * @print: prints
- */
-typedef struct printer
-{
-	char *symbol;
-	void (*print)(va_list arg);
-} printer_t;
-
-int sum_them_all(unsigned int n, ...);
-
+int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-
-void print_strings(const char *seperator, const unsigned int n, ...);
-
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 #endif
